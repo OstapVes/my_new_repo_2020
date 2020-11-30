59ae5907a86347c56015659a8f5d9d79eb10a7e4
Ostap Vesolovskyi <ostap.vesolovskyi.knm.2020@lpnu.ua>
2020-11-30 15:15:16 +0200

    my first commit

---
+++ Lab3.c
@@ -0,0 +1,15 @@
+﻿"Lab3.exe" (Win32). Загружено "C:\Users\user\source\repos\ConsoleApplication9\Debug\Lab3.exe". Символы загружены.
+"Lab3.exe" (Win32). Загружено "C:\Windows\SysWOW64\ntdll.dll". 
+"Lab3.exe" (Win32). Загружено "C:\Windows\SysWOW64\kernel32.dll". 
+"Lab3.exe" (Win32). Загружено "C:\Windows\SysWOW64\KernelBase.dll". 
+"Lab3.exe" (Win32). Загружено "C:\Program Files\Avast Software\Avast\x86\aswhook.dll". 
+"Lab3.exe" (Win32). Загружено "C:\Windows\SysWOW64\vcruntime140d.dll". 
+"Lab3.exe" (Win32). Загружено "C:\Windows\SysWOW64\ucrtbased.dll". 
+"Lab3.exe" (Win32). Загружено "C:\Windows\SysWOW64\api-ms-win-core-localization-l1-2-0.dll". 
+"Lab3.exe" (Win32). Загружено "C:\Windows\SysWOW64\api-ms-win-core-processthreads-l1-1-1.dll". 
+"Lab3.exe" (Win32). Загружено "C:\Windows\SysWOW64\api-ms-win-core-file-l1-2-0.dll". 
+"Lab3.exe" (Win32). Загружено "C:\Windows\SysWOW64\api-ms-win-core-timezone-l1-1-0.dll". 
+"Lab3.exe" (Win32). Загружено "C:\Windows\SysWOW64\api-ms-win-core-file-l2-1-0.dll". 
+"Lab3.exe" (Win32). Загружено "C:\Windows\SysWOW64\api-ms-win-core-synch-l1-2-0.dll". 
+Поток 0xf3c завершился с кодом 0 (0x0).
+Программа "[10460] Lab3.exe" завершилась с кодом 0 (0x0).
